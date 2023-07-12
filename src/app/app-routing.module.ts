3093f16cc992242f128866c68482ef6e901bd39d
Romit Dobariya <romitdobariya5075@gmail.com>
2023-07-12 12:55:15 +0530

    chil change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,4 +26,10 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+constructor(){
+
+}
+
+}
